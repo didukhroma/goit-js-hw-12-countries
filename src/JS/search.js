1868a66444a5adbc87f81ffc29e12734a83756e2
didukhroma <70459354+didukhroma@users.noreply.github.com>
2021-03-04 12:22:39 +0200

    full work version

--- src/JS/search.js
+++ src/JS/search.js
@@ -1,33 +1,47 @@
 import _debounce from 'lodash';
-import '../../node_modules/@pnotify/core/dist/Material.css';
-import { alert } from '../../node_modules/@pnotify/core/dist/PNotify';
-// console.dir(PNotify);
-// import { alert } from '@pnotify/core';
+import { error } from '@pnotify/core';
 import createBaseMarkup from './createBaseMarkup.js';
 import oneCountryTemplate from '../template/oneCountryTemplate.hbs';
 import manyCountryTemplate from '../template/manyCountryTemplate.hbs';
 import createMarkup from './createMarkup';
 import API from './fetchCountries';
 createBaseMarkup();
+//-------------------------------------
+// const Handlebars = require('handlebars');
+// Handlebars.registerHelper('lengthOfLanguage', function (value) {
+//   return value.length > 1;
+// });
+//-------------------------------------
 const refs = {
-  boxRef: document.querySelector('.search-box'),
+  boxRef: document.querySelector('.search-result'),
   inputRef: document.querySelector('#js-input'),
 };
 //----------------
-let searchQuery;
+// let searchQuery = null;
 refs.inputRef.addEventListener('input', _.debounce(onInputChange, 1000));
+//----------------------------------------------------------------------
 function onInputChange(event) {
-  searchQuery = event.target.value;
+  let searchQuery = event.target.value;
+  if (!searchQuery) return; //фикс ошибки после ввода значения в строку-поиска и удаления ввода
   API.fetchCountries(searchQuery).then(result => {
-    console.log(result);
     if (result.length === 1) {
-      createMarkup(refs.boxRef, oneCountryTemplate(result[0]));
+      const { languages } = result[0];
+      let isOneNationalLanguage = languages.length === 1;
+      createMarkup(
+        refs.boxRef,
+        oneCountryTemplate({ isOneNationalLanguage, ...result[0] }),
+      );
     } else if (result.length >= 2 && result.length <= 10) {
       createMarkup(refs.boxRef, manyCountryTemplate(result));
     } else {
-      alert({
+      createMarkup(refs.boxRef, '');
+      error({
         title: 'Oh No!',
-        text: 'Something terrible happened.',
+        text:
+          "Something really terrible happened. You really need to read this, so I won't close automatically.",
+        hide: false,
+        sticker: false,
+        icon: false,
       });
     }
     // alert('incorect input')
